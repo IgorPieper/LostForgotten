e0f9ccaea8993fd5aada991b62334ae54b4fcec4
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2025-06-29 22:42:22 +0200

    Wsparcie do dodawania przedmiotów i Rubin

--- src/main/java/net/lostforgotten/LostForgotten.java
+++ src/main/java/net/lostforgotten/LostForgotten.java
@@ -2,6 +2,7 @@ package net.lostforgotten;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.lostforgotten.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +13,6 @@ public class LostForgotten implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/lostforgotten/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.lostforgotten.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.lostforgotten.LostForgotten;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    // Definiowanie nowego przedmiotu
+    public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(LostForgotten.MOD_ID,"ruby")))));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(LostForgotten.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        LostForgotten.LOGGER.info("Registering Mod Items for " + LostForgotten.MOD_ID);
+
+        // Dodawanie przedmiotu do trybu Creatywnego
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(RUBY);
+        });
+    }
+}
