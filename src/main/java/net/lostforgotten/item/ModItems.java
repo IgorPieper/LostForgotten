e817af4417933b290ec4fdffe99cdcdc37aafb32
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2025-06-29 23:02:29 +0200

    Wosk!

--- src/main/java/net/lostforgotten/item/ModItems.java
+++ src/main/java/net/lostforgotten/item/ModItems.java
@@ -13,6 +13,7 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     // Definiowanie nowego przedmiotu
     public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(LostForgotten.MOD_ID,"ruby")))));
+    public static final Item CRYSTALLIZED_HONEY = registerItem("crystallized_honey", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(LostForgotten.MOD_ID,"crystallized_honey")))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(LostForgotten.MOD_ID, name), item);
@@ -24,6 +25,7 @@ public class ModItems {
         // Dodawanie przedmiotu do trybu Creatywnego
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(RUBY);
+            entries.add(CRYSTALLIZED_HONEY);
         });
     }
 }
